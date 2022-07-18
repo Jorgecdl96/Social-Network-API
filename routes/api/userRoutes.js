e92b2952f40648faa78c0231ad004e096ef94a65
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-07-17 20:57:53 -0600

    route has been updated

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getOneUser,
+  createUser
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getOneUser);
+
+// .put(updateUser)
+
+module.exports = router;
